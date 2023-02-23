5e6ee4351891ae4e1100de5c4951762aedea465f
laaouatni <laaouatni.anas@outlook.it>
2023-02-23 21:20:07 +0100

    xScale, yScale Boiderplate + readme warning

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# FreeCodeCamp-Data-Visualization-Projects-
+# FreeCodeCamp-Data-Visualization-Projects
 Projects of FreeCodeCamp's Data Visualization Projects, written in D3.js and HTML/CSS
+
+this is not finished, I am still doing the projects. (soon it will include correct solution so for now don't use it)
--- Visualize-Data-with-a-Bar-Chart/bar-chart/main.js
+++ Visualize-Data-with-a-Bar-Chart/bar-chart/main.js
@@ -3,53 +3,55 @@ import "./dist/output.css";
 
 import * as d3 from "d3";
 
-// const dataset = d3.json(
-//   "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json",
-// );
+const dataset = d3
+  .json(
+    "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json",
+  )
+  .then((data) => data.data);
 
-const dataset = [
-  [34, 78],
-  [109, 280],
-  [310, 120],
-  [79, 411],
-  [420, 220],
-  [233, 145],
-  [333, 96],
-  [222, 333],
-  [78, 320],
-  [21, 123],
-];
+dataset.then((data) => {
+  const w = 500;
+  const h = 500;
 
-const w = 500;
-const h = 500;
+  const padding = 60;
 
-const padding = 60;
+  const xScale = d3
+    .scaleLinear()
+    .domain([
+      d3.min(data, (d) => d[0].split("-")[0]),
+      d3.max(data, (d) => d[0].split("-")[0]),
+    ])
+    .range([padding, w - padding]);
 
-const xScale = d3
-  .scaleLinear()
-  .domain([0, d3.max(dataset, (d) => d[0])])
-  .range([padding, w - padding]);
+  const yScale = d3
+    .scaleLinear()
+    .domain([0, d3.max(data, (d) => d[1])])
+    .range([h - padding, padding]);
 
-const yScale = d3
-  .scaleLinear()
-  .domain([0, d3.max(dataset, (d) => d[1])])
-  .range([h - padding, padding]);
+  const svg = d3
+    .select("#chart-container")
+    .append("svg")
+    .attr("width", w)
+    .attr("height", h);
 
-const svg = d3
-  .select("#chart-container")
-  .append("svg")
-  .attr("width", w)
-  .attr("height", h);
+  const xAxis = d3.axisBottom(xScale);
+  const yAxis = d3.axisLeft(yScale);
 
-const xAxis = d3.axisBottom(xScale);
-const yAxis = d3.axisLeft(yScale);
+  svg.selectAll("rect").data(data).enter().append("rect")
+    .attr("class", "bar")
+    .attr("width", (d, i) => xScale())
+    .attr("height", (d, i) => yScale())
+    .attr("x", (d, i) => xScale())
+    .attr("y", (d, i) => yScale())
+    .style("fill", "red")
+    
+  svg
+    .append("g")
+    .attr("transform", "translate(0," + (h - padding) + ")")
+    .call(xAxis);
 
-svg
-  .append("g")
-  .attr("transform", "translate(0," + (h - padding) + ")")
-  .call(xAxis);
-
-svg
-  .append("g")
-  .attr("transform", "translate(" + padding + ",0)")
-  .call(yAxis);
+  svg
+    .append("g")
+    .attr("transform", "translate(" + padding + ",0)")
+    .call(yAxis);
+});
