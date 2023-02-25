975336b309dca284e3e1f9f5faedff2d1860e161
laaouatni <laaouatni.anas@outlook.it>
2023-02-25 22:55:33 +0100

    finished first project

--- Visualize-Data-with-a-Bar-Chart/bar-chart/main.js
+++ Visualize-Data-with-a-Bar-Chart/bar-chart/main.js
@@ -10,16 +10,16 @@ const dataset = d3
   .then((data) => data.data);
 
 dataset.then((data) => {
-  const w = 500;
-  const h = 500;
+  const w = window.innerWidth * 0.85;
+  const h = window.innerHeight * 0.6;
 
   const padding = 60;
 
   const xScale = d3
     .scaleLinear()
     .domain([
-      d3.min(data, (d) => d[0].split("-")[0]),
-      d3.max(data, (d) => d[0].split("-")[0]),
+      d3.min(data, (d) => getYear(d[0])),
+      d3.max(data, (d) => getYear(d[0])),
     ])
     .range([padding, w - padding]);
 
@@ -37,14 +37,35 @@ dataset.then((data) => {
   const xAxis = d3.axisBottom(xScale);
   const yAxis = d3.axisLeft(yScale);
 
-  svg.selectAll("rect").data(data).enter().append("rect")
+  const graphHeight = h - padding;
+  const graphWidth = w - padding * 2;
+
+  const widthPerBar = graphWidth / data.length;
+
+  svg
+    .selectAll("rect")
+    .data(data)
+    .enter()
+    .append("rect")
     .attr("class", "bar")
-    .attr("width", (d, i) => xScale())
-    .attr("height", (d, i) => yScale())
-    .attr("x", (d, i) => xScale())
-    .attr("y", (d, i) => yScale())
-    .style("fill", "red")
-    
+    .attr("width", () => widthPerBar)
+    .attr("height", (d, i) => graphHeight - yScale(d[1]))
+    .attr("x", (d, i) => i * widthPerBar + padding)
+    .attr("y", (d) => yScale(d[1]))
+    .attr("data-date", (d) => d[0])
+    .attr("data-gdp", (d) => d[1])
+    .on("mouseover", (mouseData, d) => {
+      const tooltip = document.querySelector("#tooltip");
+      const tooltipDate = document.querySelector("#tooltip-date");
+      const tooltipGDP = document.querySelector("#tooltip-gdp");
+      // center tooltip
+      tooltip.style.left = mouseData.pageX - tooltip.offsetWidth + "px";
+      tooltipDate.textContent = d[0];
+      tooltipGDP.textContent = `$${d[1]} Billion`;
+      tooltip.classList.remove("opactiy-0");
+      tooltip.style.opacity = 1;
+    });
+
   svg
     .append("g")
     .attr("transform", "translate(0," + (h - padding) + ")")
@@ -55,3 +76,7 @@ dataset.then((data) => {
     .attr("transform", "translate(" + padding + ",0)")
     .call(yAxis);
 });
+
+function getYear(str) {
+  return str.split("-")[0];
+}
