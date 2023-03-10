b6dc7fc428f9f9a5455405cfcf1196ce527bbae2
laaouatni <laaouatni.anas@outlook.it>
2023-03-10 22:49:10 +0100

    save commit... tomorrrow we will finish the third project of D3 freecodecamp

--- Visualize-Data-with-a-Heat-Map/heat-map/main.js
+++ Visualize-Data-with-a-Heat-Map/heat-map/main.js
@@ -50,37 +50,82 @@ json.then((data) => {
       `translate(${p / 2}px, ${p / 2}px
       )`,
     )
-    .call(d3.axisLeft(yScale).tickFormat((d) => {
-      const date = new Date();
-      date.setMonth(d - 1);
-      return d3.timeFormat("%B")(date);
-    }));
-  
+    .call(
+      d3.axisLeft(yScale).tickFormat((d) => {
+        const date = new Date();
+        date.setMonth(d - 1);
+        return d3.timeFormat("%B")(date);
+      }),
+    );
+
   const allLengths = returnAllLengthsMonths(dataset);
 
   rects
     .attr("class", "cell")
-    .attr("fill", (d) => randomColor())
     .attr("x", (d) => xScale(returnYear(d.year)))
     .attr("y", (d) => yScale(d.month))
     .attr("width", (d) => {
       const month = d.month;
-      const totalItemsPerMonth = allLengths.find((item) => item.monthNumber === month);
+      const totalItemsPerMonth = allLengths.find(
+        (item) => item.monthNumber === month,
+      );
       return w / totalItemsPerMonth.length;
     })
     .attr("height", yScale.bandwidth())
     .attr("data-month", (d) => d.month - 1)
     .attr("data-year", (d) => d.year)
-    .attr("data-temp", (d) => d.variance)
-  
-  // description
-  d3.select("#description")
-    .text(() => {
-      const minYear = d3.min(dataset, (d) => d.year);
-      const maxYear = d3.max(dataset, (d) => d.year);
-      const baseTemperature = data.baseTemperature;
-      return `${minYear} - ${maxYear}: base temperature ${baseTemperature}℃`;
+    .attr("data-temp", (d) => data.baseTemperature + d.variance);
+
+  rects.on("mouseover", (d, i, n) => { 
+    d3.select(d.target)
+    /* add tooltip functionality tomorrow */
+  });
+
+  rects
+    .classed("fill-blue-600", (d) => {
+      const temperature = data.baseTemperature + d.variance;
+      return temperature < 3.9;
+    })
+    .classed("fill-blue-500", (d) => {
+      const temperature = data.baseTemperature + d.variance;
+      return temperature >= 3.9 && temperature < 5;
+    })
+    .classed("fill-blue-400", (d) => {
+      const temperature = data.baseTemperature + d.variance;
+      return temperature >= 5 && temperature < 6.1;
+    })
+    .classed("fill-blue-300", (d) => {
+      const temperature = data.baseTemperature + d.variance;
+      return temperature >= 6.1 && temperature < 7.2;
+    })
+    .classed("fill-red-200", (d) => {
+      const temperature = data.baseTemperature + d.variance;
+      return temperature >= 7.2 && temperature < 8.3;
+    })
+    .classed("fill-red-300", (d) => {
+      const temperature = data.baseTemperature + d.variance;
+      return temperature >= 8.3 && temperature < 9.5;
+    })
+    .classed("fill-red-400", (d) => {
+      const temperature = data.baseTemperature + d.variance;
+      return temperature >= 9.5 && temperature < 10.6;
+    })
+    .classed("fill-red-500", (d) => {
+      const temperature = data.baseTemperature + d.variance;
+      return temperature >= 10.6 && temperature < 11.7;
+    })
+    .classed("fill-red-600", (d) => {
+      const temperature = data.baseTemperature + d.variance;
+      return temperature >= 11.7;
     });
+
+  // description
+  d3.select("#description").text(() => {
+    const minYear = d3.min(dataset, (d) => d.year);
+    const maxYear = d3.max(dataset, (d) => d.year);
+    const baseTemperature = data.baseTemperature;
+    return `${minYear} - ${maxYear}: base temperature ${baseTemperature}℃`;
+  });
 });
 
 function returnYear(year) {
