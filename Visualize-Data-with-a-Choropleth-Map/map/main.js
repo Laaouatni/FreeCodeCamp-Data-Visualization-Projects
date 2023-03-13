5f82a0a4ebb00607e43a61582046bcdb780155ee
laaouatni <laaouatni.anas@outlook.it>
2023-03-13 19:41:06 +0100

    created axisBottom and axisLeft

--- Visualize-Data-with-a-Choropleth-Map/map/main.js
+++ Visualize-Data-with-a-Choropleth-Map/map/main.js
@@ -14,9 +14,29 @@ const multipleFetch = Promise.all([
 multipleFetch.then(([USeducationData, UScountiesMapData]) => {
   const svg = d3.select("svg");
 
-  const width = window.innerWidth / 100 * 60;
-  const height = window.innerHeight / 100 * 60;
-  
-  svg.attr("width", width).attr("height", height);
-});
+  const w = (window.innerWidth / 100) * 60;
+  const h = (window.innerHeight / 100) * 60;
+  const p = 60;
 
+  svg.attr("width", w).attr("height", h);
+
+  const xScale = d3
+    .scaleLinear()
+    .domain([0, 100])
+    .range([0, w - p]);
+
+  svg
+    .append("g")
+    .style("transform", `translate(${p / 2}px, ${h - p / 2}px)`)
+    .call(d3.axisBottom(xScale));
+
+  const yScale = d3
+    .scaleLinear()
+    .domain([0, 100])
+    .range([h-p, 0]);
+
+  svg
+    .append("g")
+    .style("transform", `translate(${p / 2}px, ${p / 2}px)`)
+    .call(d3.axisLeft(yScale));
+});
