4fc4b76d58c06409f203aa708465d7c81b8934bc
laaouatni <laaouatni.anas@outlook.it>
2023-03-14 22:08:37 +0100

    created the legend

--- Visualize-Data-with-a-Choropleth-Map/map/main.js
+++ Visualize-Data-with-a-Choropleth-Map/map/main.js
@@ -44,36 +44,48 @@ multipleFetch.then(([USeducationData, UScountiesMapData]) => {
 
   const twColorObj = {
     "fill-green-100": {
+      max: 12,
       needToBeApplied: (percentage) => percentage < 12,
     },
     "fill-green-200": {
+      min: 12,
+      max: 21,
       needToBeApplied: (percentage) =>
         percentage >= 12 && percentage < 21,
     },
     "fill-green-300": {
+      min: 21,
+      max: 30,
       needToBeApplied: (percentage) =>
         percentage >= 21 && percentage < 30,
     },
     "fill-green-400": {
+      min: 30,
+      max: 39,
       needToBeApplied: (percentage) =>
         percentage >= 30 && percentage < 39,
     },
     "fill-green-500": {
+      min: 39,
+      max: 48,
       needToBeApplied: (percentage) =>
         percentage >= 39 && percentage < 48,
     },
     "fill-green-600": {
+      min: 48,
+      max: 66,
       needToBeApplied: (percentage) =>
         percentage >= 48 && percentage < 66,
     },
     "fill-green-700": {
+      min: 66,
       needToBeApplied: (percentage) => percentage >= 66,
     },
   };
 
   const legend = d3.select("#legend");
 
-  legend.classed("flex gap-4 flex-wrap justify-center", true)
+  legend.classed("flex gap-4 flex-wrap justify-center rounded", true);
 
   const legendChilds = legend
     .selectAll("div")
@@ -82,8 +94,20 @@ multipleFetch.then(([USeducationData, UScountiesMapData]) => {
     .append("div");
 
   legendChilds
-    .classed("border p-4", true)
-    .text((d) => d);
+    .classed("border p-4 flex items-center gap-4 rounded-lg hover:shadow transition", true)
+    .html((d) => {
+      const bg = d.replace("fill", "bg");
+      const value = twColorObj[d];
+
+      return `
+        <div class="h-4 w-4 ${bg}"></div>
+        <div>
+          ${value.min ? `${value.min}%` : "<"}
+          ${value.min && `${value.max}%` ? "-" : ""} 
+          ${value.max ? `${value.max}%` : ">"}
+        </div>
+      `;
+    });
 
   Object.keys(twColorObj).forEach((key) => {
     const value = twColorObj[key];
